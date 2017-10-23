b170affbd97a8e3d6a84d08496fc4031e3fed7ff
soulpoison <Zz_Arrogance@126.com>
2017-10-23 13:46:43 +0800

    echarts源文件引入

---
+++ u-project/models/User.js
@@ -0,0 +1,36 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * User Model
+ * ==========
+ */
+var User = new keystone.List('User', {
+	label: '用户管理',
+});
+
+User.add({
+	name: { type: Types.Name, required: true, index: true, label: '用户名' },
+	email: { type: Types.Email, initial: true, required: true, unique: true, index: true, label: '邮箱' },
+	nickname: { type: Types.Text, initial: true, index: true, label: '姓名' },
+	password: { type: Types.Password, initial: true, required: true, label: '密码' },
+	phoneNumber: { type: Types.Number, index: true, label: '手机号' },
+	gender: {
+		label: '性别',
+		type: Types.Select,
+		options: [{ value: 'male', label: '男' }, { value: 'female', label: '女' }],
+		default: 'male',
+	},
+});
+
+// Provide access to Keystone
+User.schema.virtual('canAccessKeystone').get(function () {
+	return this.isAdmin;
+});
+User.relationship({ path: 'angel', ref: 'Angel', refPath: 'owner' });
+
+/**
+ * Registration
+ */
+User.defaultColumns = 'name, email, phoneNumber';
+User.register();
